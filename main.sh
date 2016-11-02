25984b84be2f4524747da8dd526ef9d3e399415c
milt0n <cedric.lood@kuleuven.be>
2016-11-02 13:56:00 +0100

    quality control of reads

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-readonly SOFTWARE_REQUIRED=(fastq-dump spades.py ht-trim ht-filter)
+readonly SOFTWARE_REQUIRED=(fastq-dump spades.py ht-trim ht-filter fastqc)
 readonly SRA_ACCESSION="SraAccList.txt"
 readonly ARG="$1"
-readonly SRA_FOLDER="sra"
+readonly SRA_FOLDER="/mnt/data/CbGenomics/"
 
 # progress prompt
 progress_prompt(){
@@ -13,6 +13,7 @@ progress_prompt(){
 usage(){
     echo "Usage : $0 [retrieve_sra | qc_fastq | assembly | annotation ]"
 }
+
 # checks if a given software is available on the system (not testing for specific version)
 is_installed() {
     local software=$1
@@ -39,14 +40,42 @@ software_checklist() {
 retrieve_sra(){
     progress_prompt "retrieving SRA files"
     while IFS= read -r line; do
-	#echo "$line"
-	fastq-dump -v --split-files --outdir $SRA_FOLDER $line
+	fastq-dump -v --split-files --outdir $SRA_FOLDER/$line $line
     done < "$SRA_ACCESSION"
 }
 
 # performs the QC of the reads
 qc_fastq() {
-    echo "todo"
+    while IFS= read -r line; do
+	# Reports of pre-processing quality control of fastq files
+	progress_prompt "pre-processing quality control"
+	mkdir $SRA_FOLDER/$line/pre
+	fastqc --outdir $SRA_FOLDER/$line/pre --extract --threads 4 \
+	       $SRA_FOLDER/$line/${line}_1.fastq \
+	       $SRA_FOLDER/$line/${line}_2.fastq
+
+	# QC of the reads (Trimming)
+	progress_prompt "Trimming"
+	ht-trim -i $SRA_FOLDER/$line/${line}_1.fastq \
+		-o $SRA_FOLDER/$line/${line}_1_trim.fastq
+	ht-trim -i $SRA_FOLDER/$line/${line}_2.fastq \
+		-o $SRA_FOLDER/$line/${line}_2_trim.fastq
+
+	# QC of the reads (size exclusion)
+	progress_prompt "Size exclusion"
+	ht-filter --paired --filter length \
+		  -i $SRA_FOLDER/$line/${line}_1_trim.fastq \
+		     $SRA_FOLDER/$line/${line}_2_trim.fastq \
+		  -o $SRA_FOLDER/$line/${line}_final.fastq
+
+	# Reports of post-processing QC of the fastq files
+	progress_prompt "post-processing quality control"
+	mkdir $SRA_FOLDER/$line/post
+	fastqc --outdir $SRA_FOLDER/$line/post --extract --threads 4 \
+	       $SRA_FOLDER/$line/${line}_final_1.fastq \
+	       $SRA_FOLDER/$line/${line}_final_2.fastq \
+	       $SRA_FOLDER/$line/${line}_final_s.fastq
+    done < "$SRA_ACCESSION"
 }
 
 # denovo assembly with spades
@@ -59,7 +88,7 @@ main() {
     then
      	usage ; exit ;
     fi
-    
+
     software_checklist
 
     case "$ARG" in
