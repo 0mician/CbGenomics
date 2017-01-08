cc30bd4fe13ab3c503776651b278bb9512a6c07b
milt0n <cedric.lood@kuleuven.be>
2017-01-08 17:07:31 +0100

    plop

--- get_genes_nt.py
+++ get_genes_nt.py
@@ -1,6 +1,7 @@
 import os
 import json
 import pprint
+from textwrap import wrap
 
 from Bio import Entrez
 from Bio import SeqIO
@@ -8,32 +9,54 @@ from Bio import SeqIO
 Entrez.email = "clo@kuleuven.be"
 fasta = "genes_nt.fasta"
 gene_list = os.path.join("data", "genes.txt")
+fasta_out = os.path.join("data", "genes.fasta")
+genes_not_found_out = os.path.join("data", "genes_not_found.txt")
 
-with open(gene_list, "r") as f:
-    for gene in f:
-        print(gene)
-        gene_handle = Entrez.esearch(db="gene", term=gene)
+fasta_fh = open(fasta_out, "w") # file handler for output genes (fasta)
+genes_not_found_fh = open(genes_not_found_out, "w")
+
+with open(gene_list, "r") as gene_fh:
+    for gene in gene_fh:
+        
+        # search first in gene database to get a list of ID
+        gene_handle = Entrez.esearch(db="gene", term=gene.strip("\n")+"[sym]")
         gene_record = Entrez.read(gene_handle)
         gene_id_list = gene_record['IdList']
+
+        # any results?
         if(gene_id_list):
+
+            # go through each ID and get linked genome and genomic region
             for gene_id in gene_id_list:
-                summary_handle = Entrez.esummary(db="gene", id=5184378, report="full")
+                summary_handle = Entrez.esummary(db="gene", id=gene_id, report="full")
                 summary_record = Entrez.read(summary_handle)['DocumentSummarySet']['DocumentSummary'][0]
-                genomic_info = summary['GenomicInfo'][0]
+                genomic_info = summary_record['GenomicInfo'][0]
+                gene_version = summary_record['Status']
                 genome_accession = genomic_info['ChrAccVer']
-                chr_start = genomic_info['ChrStart']
-                chr_stop = genomic_info['ChrStop']
-                print(genome_accession + " " + chr_start + " " + chr_stop)
-                nt_handle = Entrez.efetch(db="nucleotide",
-                                          id=genome_accession,
-                                          rettype="fasta",
-                                          strand=1,
-                                          seq_start=chr_start,
-                                          seq_stop=chr_stop)
-                nt_record = SeqIO.read(nt_handle, "fasta")
-                handle_nt.close()
-                break
+                chr_start = int(genomic_info['ChrStart'])
+                chr_stop = int(genomic_info['ChrStop'])
+                if(gene_version == "0"):
+                    fasta_fh.write(">" + gene.strip("\n") + "@" + genome_accession + ":"
+                                   + str(chr_start) + ":" + str(chr_stop) + "\n")
+                    
+                    # get actual sequence from the genomic region of interest
+                    nt_handle = Entrez.efetch(db="nucleotide",
+                                              id=genome_accession,
+                                              rettype="fasta",
+                                              strand=1,
+                                              seq_start=chr_start+1,
+                                              seq_stop=chr_stop+1)
+                    nt_record = SeqIO.read(nt_handle, "fasta")
+                    nt_handle.close()
+                    fasta_fh.write("\n".join(wrap(str(nt_record.seq), 80)) + "\n")
+                else:
+                    genes_not_found_fh.write(gene.strip("\n") + ":old_version\n")
+                print(nt_record.id)
+
+        # empty list (based on gene label, no results could be found, literature)
         else:
-            print("nothing here")
-        
-        break
+            genes_not_found_fh.write(gene.strip("\n"))
+
+gene_fh.close()
+genes_not_found_fh.close()
+fasta_fh.close()
